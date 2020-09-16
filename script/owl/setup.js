663328ae23b781234182be9c04ebe709f58a787c
Jayneanderson <jayneanderson.santos@gmail.com>
2020-09-16 00:21:42 -0300

    criando o carousel e desenvolvendo parte de contato

---
+++ script/owl/setup.js
@@ -0,0 +1,48 @@
+/* $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+ */
+
+
+
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items:4,
+        loop:true,
+        margin:10,
+       /* nav: true,
+        navText: ['Prev', 'For'], */
+        autoplay:true,
+        autoplayTimeout:1000,/* ,
+        autoplayHoverPause:true */
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+    $('.play').on('click',function(){
+        owl.trigger('play.owl.autoplay',[1000])
+    })
+    $('.stop').on('click',function(){
+        owl.trigger('stop.owl.autoplay')
+    })
